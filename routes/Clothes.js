f97332c8958196a0a4d667a2e54021f5d6359410
hira9206 <snowstar150@gmail.com>
2020-06-15 15:30:33 +0800

    Initial Clothes Routes

---
+++ routes/Clothes.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const Clothes = require('../models/clothes')
+const router = express.Router()
+
+// All Clothes 
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+      const clothes = await Clothes.find(searchOptions)
+      res.render('clothes/index', { 
+          clothes : clothes, 
+          searchOptions: req.query
+        })
+    } catch {
+      res.redirect('/')
+    }
+})
+
+// New Clothes 
+router.get('/new', (req, res) => {
+    res.render('clothes/new', { clothes: new Clothes() })
+})
+
+// Create Clothes 
+router.post('/', async (req, res) => {
+    const clothes = new Clothes({
+        name: req.body.name
+    })
+    try{
+        const newClothes = await clothes.save()
+        //res.redirect('clothes/${newClothes.id}')
+        res.redirect('clothes')
+    } catch {
+        res.render('clothes/new', {
+            clothes: clothes,
+            errorMessage: 'Error creating Clothes'
+        })
+    }
+})
+
+module.exports = router
