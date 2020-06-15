3d3e1d4f4422e55c2d7142995019397761626f2d
hira9206 <snowstar150@gmail.com>
2020-06-15 22:40:25 +0800

    heroku problem

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-app.use(express.json());
-app.use(express.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
